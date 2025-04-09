6ca8050adf349f2f88fb482604c2c52a976b0353
uemoo <a_uemura@yumemi.co.jp>
2025-04-09 18:49:11 +0900

    ✨ Add weather forecast screen layout

---
+++ lib/component/forecast_text_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ForecastTextButton extends StatelessWidget {
+  const ForecastTextButton({
+    required this.label,
+    required this.color,
+    required this.onPressed,
+    super.key,
+  });
+
+  final String label;
+  final Color color;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      child: Text(
+        label,
+        textAlign: TextAlign.center,
+        style: TextStyle(color: color),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('label', label));
+    properties.add(ColorProperty('color', color));
+    properties.add(ObjectFlagProperty('onPressed', onPressed));
+  }
+}
